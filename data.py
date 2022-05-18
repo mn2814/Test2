bd1ba83ceb44c30784ed5f061947bb77d22155a1
Morgan <mn2814@ic.ac.uk>
2022-05-18 14:24:23 +0100

    Updated

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("example_data.csv",delimeter = ",",skiprows=1)
+data = np.loadtxt("example_data.csv",delimiter = ",",skiprows=1)
 
 plt.plot(data[:,0], data[:,1])
 plt.show()
